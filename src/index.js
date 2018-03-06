23d36f3f35638c2e0318660c7e1d1249e819c220
Dmitry Petrov <dpetrov@competentum.ru>
2018-03-06 22:30:19 +0300

    [CGUIC-31] (feat) add some user settings checks and early dnd-functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import EventEmitter from 'events';
 import merge from 'merge';
 import utils from 'cg-component-utils';
 
+/* For ES-Lint comment
 const DND_CLASS = 'cg-dnd';
 
 const CLASS = {
@@ -14,7 +15,7 @@ const CLASS = {
 const KEY_CODE = {
   ESC: 27,
   TAB: 9
-};
+}; */
 
 /**
  * Accessible DnD Component
@@ -29,6 +30,27 @@ class CgDnd extends EventEmitter {
     if (!this._DEFAULT_SETTINGS) {
       this._DEFAULT_SETTINGS = {
         disabled: false,
+        bounds: '',
+        helper: 'original',
+        handler: '',
+        snap: true,
+        maxItemsInDropArea: 1,
+        dragItems: [
+          {
+            node: '',
+            data: null,
+            ariaLabel: '',
+            className: ''
+          }
+        ],
+        dropAreas: [
+          {
+            node: '',
+            ariaLabel: '',
+            data: null,
+            className: ''
+          }
+        ]
       };
     }
 
@@ -45,6 +67,7 @@ class CgDnd extends EventEmitter {
     if (!this._EVENTS) {
       this._EVENTS = {
         CREATE: 'create',
+        DRAG_START: 'drag_start'
       };
     }
 
@@ -60,6 +83,8 @@ class CgDnd extends EventEmitter {
 
     this._applySettings(settings);
 
+    this._setHandlersForEachDragElems();
+
     this._render();
 
     this._addListeners();
@@ -70,7 +95,63 @@ class CgDnd extends EventEmitter {
    * @private
    */
   _addListeners() {
+    this.settings.dragItems.forEach((item) => {
+      item.dragHandler.addEventListener('mousedown', (e) => this.emit(this.constructor.EVENTS.DRAG_START, e, item));
+    });
+    this._addCustomListeners();
+  }
 
+  _addCustomListeners() {
+    this.on(this.constructor.EVENTS.DRAG_START, (e, item) => {
+      const draggedNode = item.node;
+      const box = item.node.getBoundingClientRect();
+      const shiftX = e.pageX - box.left + draggedNode.offsetLeft;
+      const shiftY = e.pageY - box.top + draggedNode.offsetTop;
+
+      /* With document correct dnd
+      const clientHeight = this.settings.bounds.documentElement.clientHeight;
+      const clientWidth = this.settings.bounds.documentElement.clientWidth;
+      const boundsLimits = {
+        minHeight: e.pageY - box.top,
+        minWidth: e.pageX - box.left,
+        maxHeight: clientHeight - (box.top + box.height - e.pageY),
+        maxWidth: clientWidth - (box.left + box.width - e.pageX)
+      };
+
+      const clientHeight = this.settings.bounds.clientHeight;
+      const clientWidth = this.settings.bounds.clientWidth;
+       */
+
+      // TODO: (early dnd) fix max height, when bounds = document, fix functionality and productivity
+      const dragContainerLimits = this.settings.bounds === document
+        ? document.documentElement.getBoundingClientRect()
+        : this.settings.bounds.getBoundingClientRect();
+      // Comment dragContainerLimits.height = this.settings.bounds === document
+      // ? this.settings.bounds.clientHeight : dragContainerLimits.height;
+
+      const boundsLimits = {
+        minHeight: dragContainerLimits.top + e.pageY - box.top,
+        minWidth: dragContainerLimits.left + e.pageX - box.left,
+        maxHeight: dragContainerLimits.height + dragContainerLimits.top - (box.top + box.height - e.pageY),
+        maxWidth: dragContainerLimits.width + dragContainerLimits.left - (box.left + box.width - e.pageX)
+      };
+
+      document.onmousemove = (e) => {
+        const xPos = e.pageX >= boundsLimits.maxWidth
+          ? boundsLimits.maxWidth : e.pageX <= boundsLimits.minWidth
+                       ? boundsLimits.minWidth : e.pageX;
+
+        const yPos = e.pageY >= boundsLimits.maxHeight
+          ? boundsLimits.maxHeight : e.pageY <= boundsLimits.minHeight
+                       ? boundsLimits.minHeight : e.pageY;
+
+        draggedNode.style.transform = `translate(${xPos - shiftX}px, ${yPos - shiftY}px)`;
+      };
+
+      document.onmouseup = () => {
+        document.onmousemove = null;
+      };
+    });
   }
 
   /**
@@ -84,20 +165,220 @@ class CgDnd extends EventEmitter {
      */
     this.settings = merge({}, this.constructor.DEFAULT_SETTINGS, settings);
 
+    for (const key in this.settings) {
+      if (this.settings.hasOwnProperty(key)) {
+        this.settings[key] = this._checkSetting(key, this.settings[key]);
+      }
+    }
+
+  }
+
+  /**
+   * Checks and fix user settings
+   * @param {string} settingName - checked property.
+   * @param {string|number|object|boolean} settingValue - checked property value
+   * @param {Element} elemNode - HTML-Element for setting HTML-attributes and HTML-classes
+   * @return {string|number|object|boolean} - return verified value
+   * @private
+   */
+  _checkSetting(settingName, settingValue, elemNode) {
+    let verifiedValue;
+    let isNodeAttrribute = false;
+    let isNodeClassName = false;
+
+    switch (settingName) {
+      case 'dragItems':
+      case 'dropAreas':
+        if (Array.isArray(settingValue) && settingValue.length) {
+          const items = [...settingValue];
+
+          items.forEach((item) => {
+            if (typeof item === 'object') {
+
+              // We search elements nodes by first, because then we set params to them
+              if (item.hasOwnProperty('node')) {
+                item.node = this._getHTMLNodeElement(item.node, true);
+              }
+
+              for (const key in item) {
+                if (item.hasOwnProperty(key) && key !== 'node') {
+                  item[key] = this._checkSetting(key, item[key], item.node);
+                }
+              }
+            }
+          });
+
+          verifiedValue = items;
+        } else {
+          this._showSettingError(settingName, settingValue, `Please set Array of ${settingName}.`);
+        }
+        break;
+      case 'className':
+        isNodeClassName = true;
+        if (typeof settingValue === 'string') {
+          verifiedValue = settingValue.replace(/^\./, '');
+        } else {
+          this._showSettingError(settingName, settingValue, 'Please set string of class name.');
+        }
+        break;
+      case 'bounds':
+
+        // TODO: translate ndoeElement in array of bounds coordinates
+
+        verifiedValue = this._getHTMLNodeElement(settingValue, true, document, true) || document;
+        break;
+      case 'helper':
+        verifiedValue = settingValue.toLowerCase() === 'clone' ? 'clone' : this.constructor.DEFAULT_SETTINGS.helper;
+        break;
+      case 'handler':
+        if (typeof settingValue === 'string') {
+          if (settingValue.length) {
+            verifiedValue = this._fixClassSelector(settingValue);
+          }
+        } else {
+          this._showSettingError(settingName, settingValue, 'Please set class selector string.');
+        }
+        break;
+      case 'snap':
+      case 'disabled':
+        verifiedValue = this._checkOnBoolean(settingValue);
+
+        if (verifiedValue === null) {
+          this._showSettingError(settingName, settingValue, 'Please set true or false.');
+        }
+        break;
+      case 'ariaLabel':
+        isNodeAttrribute = true;
+        if (typeof settingValue === 'string') {
+          verifiedValue = settingValue;
+        } else {
+          this._showSettingError(settingName, settingValue, 'Please set string.');
+        }
+        break;
+      default:
+        verifiedValue = settingValue;
+    }
+
+    if (isNodeClassName && verifiedValue.length) {
+      utils.addClass(elemNode, verifiedValue);
+    }
+    if (isNodeAttrribute && verifiedValue.length) {
+      elemNode.setAttribute('aria-label', verifiedValue);
+    }
+
+    return verifiedValue;
+  }
+
+  /**
+   * Shows error in console.
+   * @param {string} settingName - name of setting property.
+   * @param {string|number|object|boolean} settingValue - wrong value
+   * @param {string} validMessage - message, which contains correct value type
+   * @private
+   */
+  _showSettingError(settingName, settingValue, validMessage) {
+    const errorValue = typeof settingValue === 'string' ? settingValue : typeof settingValue;
+
+    throw new Error(`${errorValue} isn't valid value for ${settingName}! ${validMessage}`);
+  }
+
+  /**
+   * Set drag-handlers for each drag item, if Element was got, otherwise set drag-item as drag-handler
+   * @private
+   */
+  _setHandlersForEachDragElems() {
+    this.settings.dragItems.forEach((item) => {
+      item.dragHandler = this._getHTMLNodeElement(this.settings.handler, false, item.node, true) || item.node;
+    });
+  }
+
+  /**
+   * Checks and fix HTML Element or Selector string.
+   * @param {string|Element} value - checked string.
+   * @param {boolean} byID - if true - we'll find Element by ID, otherwise we'll find it by class.
+   * @param {Element} container - HTML-container in which we'll try find checked value
+   * @param {boolean} allowEmpty - if true, function'll get possible empty selector string, otherwise error'll show
+   * @return {Element|null} - return HTML Element node or null, if it wasn't found
+   * @private
+   */
+  _getHTMLNodeElement(value, byID, container = document, allowEmpty = false) {
+    let htmlNode = null;
+
+    if (typeof value === 'string' && value.length) {
+      const selector = byID ? this._checkIDSelector(value) : this._fixClassSelector(value);
+
+      htmlNode = container.querySelector(selector);
+    } else if (value instanceof Element) {
+      htmlNode = value;
+    }
+
+    if (!htmlNode && !allowEmpty) {
+      throw new Error(`${value} wasn't found!`);
+    }
+
+    return htmlNode;
+  }
+
+  /**
+   * Checks and fix ID selector string.
+   * @param {string} selectorString - checked string.
+   * @return {string} - return valid ID selector string
+   * @private
+   */
+  _checkIDSelector(selectorString) {
+    if (selectorString.search(/^\./) !== -1) {
+      throw new Error('ID selector was expected, but Class selector was found');
+    }
+
+    return selectorString.search(/^#/) === -1 ? `#${selectorString}` : selectorString;
+  }
+
+  /**
+   * Checks and fix class selector string.
+   * @param {string} selectorString - checked string.
+   * @return {string} - return valid class selector string
+   * @private
+   */
+  _fixClassSelector(selectorString) {
+    if (selectorString.search(/^#/) !== -1) {
+      throw new Error('Class selector was expected, but ID selector was found');
+    }
+
+    return selectorString.search(/^\./) === -1 ? `.${selectorString}` : selectorString;
+  }
+
+  /**
+   * Checks user setting's boolean values.
+   * @param {boolean|string|number} value - checked value.
+   * @return {boolean|null} - return boolean value or null
+   * @private
+   */
+  _checkOnBoolean(value) {
+    let boolElem = null;
+
+    if (typeof value === 'string') {
+      boolElem = value.search(/^true$/i) !== -1 ? true : value.search(/^false$/i) !== -1 ? false : null;
+    } else {
+      boolElem = !!value;
+    }
+
+    return boolElem;
   }
 
   /**
    * Create DOM elements
    * @private
    */
+  /* For ES-Lint comment
   _render() {
 
-  }
+  } */
 
   /**
    * Disable drag
    * @param {boolean} [disabled = true]
    */
+/* For ES-Lint comment
   disable(disabled = true) {
 
   }
@@ -108,7 +389,7 @@ class CgDnd extends EventEmitter {
 
   destroy() {
 
-  }
+  } */
 }
 
 export default CgDnd;
